93b7f5f2a93e3fb67f78d88d43518eb5012c7493
TymoteuszMiszczak <166720046+TymoteuszMiszczak@users.noreply.github.com>
2024-05-14 12:17:34 +0200

    feat: update user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crude import read, create_user, search_user, remove
+from utils.crude import read, create_user, search_user, remove, update
 from models.data import users
 
 
@@ -12,15 +12,17 @@ if __name__ == '__main__':
         print('2.Dodaj znajomego: ')
         print('3.Wyszukaj znajomego: ')
         print('4.Usuń znajmoego: ')
+        print("5. Zaktualizuj znajomego")
         menu_option:str=input('Wybierz dostępną funckję z menu: ')
         if menu_option=='0':
             break
         if menu_option == '1':
            read(users)
-
         if menu_option == '2':
             create_user(users)
         if menu_option == '3':
             search_user(users)
         if menu_option == '4':
             remove(users)
+        if menu_option == '5':
+            update(users)
